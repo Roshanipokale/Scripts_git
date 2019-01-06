a8a2e50e489c36200aeac1b5e99a96be4e73d19c
Roshani P <abc234@gmail.com>
2019-01-06 10:50:35 +0530

    modified file for stash

--- calci.py
+++ calci.py
@@ -1,11 +1,11 @@
 def add(x,y):
     pass
 def subtract(x,y):
-    return x-y
+    pass
 def multiply(x,y):
-    return x*y
+    pass
 def divide(x,y):
-    return x/y
+    pass
 def square(x,y):
     pass
 
